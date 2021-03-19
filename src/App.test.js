12dc2b280865d95222431143c9f8f3277ffc5a42
Abhijit Bhattacharya <abhijit81kol@gmail.com>
2021-03-19 10:41:32 +0530

    TDD testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import {shallow} from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders renders without crashingok', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders button", ()=>{
+
+});
+
+test("renders counter display", ()=>{
+    
+});
+
+test("counter starts at 0", ()=>{
+
+});
+
+test("clicking on the button increments counter display", ()=>{
+
 });
